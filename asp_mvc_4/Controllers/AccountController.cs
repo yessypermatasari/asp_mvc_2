f7407fd262077b70e46b799deb24fb3fbbf193c5
Yessy Permatasari <yessypermatasari08@gmail.com>
2017-01-19 15:11:47 -0800

    Add project files.

---
+++ asp_mvc_4/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using asp_mvc_4.Models.EntityManager;
+using asp_mvc_4.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace asp_mvc_4.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+                if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+
+                }
+                else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
---
+++ asp_mvc_4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_mvc_4.MvcApplication" Language="C#" %>
